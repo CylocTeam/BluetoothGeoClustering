cac4e65f42702d152ab4978a55f1abf8296b5454
Almog <almogx3@gmail.com>
2020-03-27 18:24:16 +0300

    add option to choose one of the participants devices and xlsx

--- BluetoothGeoClustering_python/main.py
+++ BluetoothGeoClustering_python/main.py
@@ -2,17 +2,25 @@ import pandas as pd
 import requests
 import copy
 import params
-import devices_to_ble_id
+import devices_ble_id_excel_dicts as dicts
 import DataParser
+import numpy as np
 
 # Experiment times
-rel_time = pd.to_datetime("2020-03-27 14:00:00") #Local time! :D
+rel_time = pd.to_datetime("2020-03-27 14:00:00")  # Local time! :D
 window_size_minutes = 120
+
+# Analyze experiment
+name = "yanay"
+experiment_file = pd.read_excel(params.excels_folder+name+'.xlsx')
+display_name = dicts.name_to_displayname[name]
+relevant_keys = [key for key in dicts.bleId.keys() if display_name.lower() in key.lower()]
 sd = pd.DataFrame([])
 # load all data
-for key in bleId:
-    current_df = DataParser.url2df(url= params.url + bleId[key])
-    #allScannedDevicesInTime parse the data
+for key in relevant_keys:
+    current_df = DataParser.url2df(url=params.url + dicts.bleId[key])
+    # allScannedDevicesInTime parse the data
     current_sd = DataParser.allScannedDevicesInTime(current_df, rel_time, window_size_minutes, display_error=0)
     sd = pd.concat([sd, current_sd], ignore_index=True)
 print('done loading experiment data')
+
--- BluetoothGeoClustering_python/params.py
+++ BluetoothGeoClustering_python/params.py
@@ -1,2 +1,3 @@
 # cloud url
 url = "https://us-central1-social-protection-46da6.cloudfunctions.net/getDeviceScans?device_id="
+excels_folder = r'20200327\experiment_documentation_'
