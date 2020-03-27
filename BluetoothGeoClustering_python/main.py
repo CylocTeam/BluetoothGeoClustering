9a184a661f04bbf5dc27ef44b03dc7ceb9a91a3e
Almog <almogx3@gmail.com>
2020-03-27 23:18:45 +0300

    remove unnecessary code from main

--- BluetoothGeoClustering_python/main.py
+++ BluetoothGeoClustering_python/main.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import requests
-import copy
+import numpy as np
 import params
 import devices_ble_id_excel_dicts as dicts
 from DataParser import DataParser
-import numpy as np
+from TagMeasurements import TagMeasurements
 
 # Experiment times
 rel_time = pd.to_datetime("2020-03-27 14:00:00")  # Local time! :D
@@ -12,16 +11,15 @@ window_size_minutes = 120
 DataParser_funcs = DataParser()
 
 # Analyze experiment
+# Get BLE data
 name = "yanay"
-experiment_file = pd.read_excel(params.excels_folder+name+'.xlsx')
-display_name = dicts.name_to_displayname[name]
-relevant_keys = [key for key in dicts.bleId.keys() if display_name.lower() in key.lower()]
-sd = pd.DataFrame([])
-# load all data
-for key in relevant_keys:
-    current_df = DataParser_funcs.url2df(url=params.url + dicts.bleId[key])
-    # allScannedDevicesInTime parse the data
-    current_sd = DataParser_funcs.allScannedDevicesInTime(current_df, rel_time, window_size_minutes, display_error=0)
-    sd = pd.concat([sd, current_sd], ignore_index=True)
-print('done loading experiment data')
+TG = TagMeasurements(name)
+TG.set_relevant_keys()
+TG.tag_measurements()
+
+
+
+
+
+
 
