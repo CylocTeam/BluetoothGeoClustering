41b99088bf5e3246c56609b18051505154ceb132
Almog <almogx3@gmail.com>
2020-03-29 22:26:45 +0300

    add groupby by 2  columns option

--- BluetoothGeoClustering_python/DataFuncs.py
+++ BluetoothGeoClustering_python/DataFuncs.py
@@ -5,13 +5,13 @@ import seaborn as sns
 
 
 class DataFuncs:
-    def create_rolling_df_in_time(self,df):
+    def create_rolling_df_in_time(self, df):
         df_index = df.copy()
         df_index.index = df_index.time
         df_index = df_index.sort_index()
         return df_index
 
-    def switcher(self,df, func, column_name, win_size, percent=None):
+    def switcher(self, df, func, column_name, win_size, percent=90):
         return {
             'mean': df[column_name].rolling(win_size, min_periods=1).mean(),
             'var': df[column_name].rolling(win_size, min_periods=1).var(),
@@ -21,11 +21,10 @@ class DataFuncs:
             'median': df[column_name].rolling(win_size, min_periods=1).median(),
             'count': df[column_name].rolling(win_size, min_periods=1).count(),
             'percentile': df[column_name].rolling(win_size, min_periods=1).apply(
-                lambda x: np.percentile(x, percent, interpolation='nearest'))
+                lambda x: np.percentile(x, percent, interpolation='nearest')),
         }[func]
 
-
-    def apply_and_add_rolling_func_to_df(self,df, func, column_name, win_size_seconds):
+    def apply_and_add_rolling_func_to_df(self, df, func, column_name, win_size_seconds):
         # """
         # Inputs:
         #         df - DataFrame time indexed
@@ -42,16 +41,24 @@ class DataFuncs:
         df[column_func_name] = x
         return df
 
-
-    def run_rolling_func_df(self,df, func, column_name_group, column_name_func, win_size_seconds):
+    def run_rolling_func_df(self, df, func, column_name_group, column_name_func, win_size_seconds):
         df_grouped = df.groupby(column_name_group)
         df_rolling = df_grouped.apply(lambda x:
-                                      self.apply_and_add_rolling_func_to_df(x, func, column_name_func, win_size_seconds))
+                                      self.apply_and_add_rolling_func_to_df(x, func, column_name_func,
+                                                                            win_size_seconds))
+        return df_rolling
+
+    def run_rolling_func_df_2_columns(self, df, func, column_name_group_first, column_name_group_second,
+                                      column_name_func, win_size_seconds):
+        df_grouped = df.groupby([column_name_group_first, column_name_group_second])
+        df_rolling = df_grouped.apply(lambda x:
+                                      self.apply_and_add_rolling_func_to_df(x, func, column_name_func,
+                                                                            win_size_seconds))
         return df_rolling
 
 
-
-
-# all_tag_measurements = pd.read_pickle(r'tag_measurements_2020_03_28.pkl')
-# all_tag_measurements = all_tag_measurements.dropna(how='any').reset_index(drop=True)
-# all_tag_rolling = run_rolling_func_df(all_tag_measurements, 'mean', 'DisplayName', 'rssi', 10)
+DataFuncsObj = DataFuncs()
+all_tag_measurements = pd.read_pickle(r'tag_measurements_2020_03_28.pkl')
+all_tag_measurements = all_tag_measurements.dropna(how='any').reset_index(drop=True)
+all_tag_rolling = DataFuncsObj.run_rolling_func_df_2_columns(all_tag_measurements, 'mean', 'DisplayName','distance', 'rssi', 10)
+# all_tag_rolling = DataFuncsObj.run_rolling_func_df(all_tag_measurements, 'mean', 'DisplayName', 'rssi', 10)
