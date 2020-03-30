bc24bb2e66109423d823d7fd3720b13eaa5e8277
Almog <almogx3@gmail.com>
2020-03-30 17:42:56 +0300

    create function data creates rolling obj for calculations

--- BluetoothGeoClustering_python/DataFuncs.py
+++ BluetoothGeoClustering_python/DataFuncs.py
@@ -58,9 +58,9 @@ class DataFuncs:
         #         win_size_seconds - int of window size in seconds
         # """
 
+        df = self.create_rolling_df_in_time(df)
         win_size = str(win_size_seconds) + 's'
         column_func_name = func + '_' + column_name
-        df = self.create_rolling_df_in_time(df)
         x = self.switcher(df, func, column_name, win_size)
         df[column_func_name] = x
         return df
@@ -79,4 +79,9 @@ class DataFuncs:
         df_rolling = df_grouped.apply(lambda x:
                                       self.apply_and_add_rolling_func_to_df(x, func, column_name_func,
                                                                             win_size_seconds))
+        return df_rolling
+
+    def create_rolling_df_obj(self, df, column_name_group_first, column_name_group_second, column_name_func,win_size):
+        df_grouped = df.groupby([column_name_group_first, column_name_group_second])
+        df_rolling = df_grouped[column_name_func].rolling(win_size, min_periods=1)
         return df_rolling
