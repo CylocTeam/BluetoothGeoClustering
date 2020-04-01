f101289b43349cce8e7f67e3cdc07e3e8ba0015b
Almog <almogx3@gmail.com>
2020-04-01 18:24:40 +0300

    concat all DB to pickle files

---
+++ BluetoothGeoClustering_python/useful_dbs/BBIL/db_params.py
@@ -0,0 +1,3 @@
+main_folder = r'D:\Documents\Projects\Bluetooth\BluetoothGeoClustering\BluetoothGeoClustering_matlab\BBIL Dataset\Parsed_Data'
+exp_folders = ['experiment1', 'experiment2']
+data_folders = ['test', 'train', 'valid']
---
+++ BluetoothGeoClustering_python/useful_dbs/BBIL/merge_all_data.py
@@ -0,0 +1,27 @@
+import os
+import glob
+import pandas as pd
+import useful_dbs.BBIL.db_params as db_params
+
+all_files = pd.DataFrame([])
+# dir all csv files
+for exp_folder in db_params.exp_folders:
+    exp_files = pd.DataFrame([])
+    for data_kind in db_params.data_folders:
+        current_folder = os.path.join(db_params.main_folder, exp_folder, data_kind)
+        files = glob.glob(current_folder + "\*_data.csv")
+        for file in files:
+            curr_table = pd.read_csv(file)
+            curr_table = curr_table.rename(columns={'Datetime': 'time'})
+            curr_table.data_kind = data_kind
+            curr_table.time = pd.to_datetime(curr_table.time)
+            start_ind = -file[::-1].find('_', 9)
+            end_ind = -len('_data.csv')
+            curr_table.DisplayName = file[start_ind:end_ind]+"_phone_BBIL"
+            all_files = pd.concat([all_files, curr_table], ignore_index=True)
+            exp_files = pd.concat([exp_files, curr_table], ignore_index=True)
+            print(file)
+    exp_files.to_pickle('tag_measurements_BBIL' + exp_folder + '.pkl')
+all_files.to_pickle('tag_measurements_BBIL_all.pkl')
+
+
