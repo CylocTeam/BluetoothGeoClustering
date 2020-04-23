73fde19d43fad928db7cd619165546ff7b008027
Almog <almogx3@gmail.com>
2020-04-23 17:00:01 +0300

    extracting data for simulation

---
+++ BluetoothGeoClustering_python/simulation/simulation.py
@@ -0,0 +1,54 @@
+import numpy
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import sys
+
+sys.path.insert(0, '..')
+import warnings
+
+warnings.filterwarnings("ignore")
+from DataFuncs import DataFuncs
+
+
+def start_pipe(df):
+    return df.copy()
+
+
+def round_distance(df, res):
+    df['round_distance'] = np.round(df["distance"] * res_meter) / res_meter
+    return df
+
+
+def rssi_not_db(df):
+    df['rssi_not_db'] = np.power(10, df['rssi'] / 10)
+    return df
+
+
+def agg_funcs_each_distance(df):
+    grouped = df.groupby('round_distance')
+    simulation_params = grouped['normalized_rssi'].agg([np.mean, np.var, np.ma.count])
+    return simulation_params
+
+
+def normalized_rssi_not_db(df, norm_distance, res_norm):
+    round_distance = np.round(df["distance"] * res_norm) / res_norm
+    mean_rssi = np.mean(df['rssi_not_db'].loc[round_distance == norm_distance])
+    df['normalized_rssi'] = df['rssi_not_db'] - mean_rssi
+    return df
+
+
+if __name__ == "__main__":
+    DB_pickle = r'../useful_dbs/BBIL/tag_measurements_BBIL_all.pkl'
+    all_tag_measurements = pd.read_pickle(DB_pickle)
+    res_meter = 0.25  # m
+    norm_distance = 1  # m
+    res_norm = res_meter
+
+    simulation_data = (all_tag_measurements.pipe(start_pipe)
+                       .pipe(rssi_not_db)
+                       .pipe(normalized_rssi_not_db, norm_distance, res_norm)
+                       .pipe(round_distance, res_meter)
+                       .pipe(agg_funcs_each_distance))
+
+    simulation_data
